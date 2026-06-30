d20b9ca61b1cea9c9e203f96f1ea992199d7c0a4
harshsahu0030 <harsh0030sahu@gmail.com>
2026-06-30 14:46:57 +0530

    fixing bugs expertise 02

--- app/(clients)/expertise/page.js
+++ app/(clients)/expertise/page.js
@@ -1,13 +1,10 @@
-import { getAllServices } from "@/api/services/getAllservices";
 import Expertise from "@/components/client/main/expertise/Expertise";
 import React from "react";
 
 const page = async () => {
-  const services = await getAllServices();
-
   return (
     <>
-      <Expertise services={services} />
+      <Expertise />
     </>
   );
 };
