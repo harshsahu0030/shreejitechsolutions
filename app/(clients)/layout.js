ccbe0dcbaca0d6fe0eb617f397f2aa49e9a8504d
harshsahu0030 <harsh0030sahu@gmail.com>
2026-07-15 00:42:09 +0530

    add lenis

--- app/(clients)/layout.js
+++ app/(clients)/layout.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/client/footer/Footer";
 import Header from "@/components/client/header/Header";
 import PageLoader from "@/components/client/providers/PageLoader";
+import SmoothScroll from "@/components/client/providers/SmoothScroll";
 
 export default function RootLayout({ children }) {
   return (
     <>
       <PageLoader>
         <Header />
+        <SmoothScroll />
         {children}
         <Footer />
       </PageLoader>
--- components/client/providers/CTA.js
+++ components/client/providers/CTA.js
@@ -13,11 +13,13 @@ const CTA = ({
       className={`flex flex-col items-center justify-center  w-full h-full  py-30 bg-(--bg-${bgColor})`}
     >
       <div className="h-full w-[80%] flex flex-col gap-10 items-center">
-        <h2 className=" text-5xl md:text-8xl font-medium text-center">
+        <h2 className="text-4xl xl:text-8xl md:text-8xl font-medium text-center">
           {label}
         </h2>
 
-        <p className="text-xl font-semibold uppercase">{description}</p>
+        <p className="text-lg font-semibold uppercase text-center">
+          {description}
+        </p>
 
         <CommonLink label="contact us" url="/contact" />
       </div>
